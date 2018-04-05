3ba7220f96bd263f6748b34c7b74bd21242b3a02
scott88lee <scott88lee@gmail.com>
2018-04-05 15:43:10 +0800

    implemented layouts and frontend ui framework plus post items

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ const cookieParser = require('cookie-parser');
 
 const pg = require('pg');
 const { Pool } = require('pg');
+
+const handlebarsConfig = {
+   extname: '.handlebars',
+   layoutsDir: 'views',
+   defaultLayout: 'layout'
+};
 //const methodOverride = require('method-override');
 
 // Initialise postgres client
@@ -28,7 +34,7 @@ pool.on('error', (err, client) => {
 // Init express app
 const app = express();
 
-app.engine('handlebars', handlebars.create().engine);
+app.engine('handlebars', handlebars.create(handlebarsConfig).engine);
 app.set('view engine', 'handlebars');
 
 app.use(cookieParser());
@@ -40,32 +46,95 @@ app.use(bodyParser.urlencoded({ extended: true }));
  * Routes
  * ===============================*/
 
+app.get('/login', (request, response) => {         // Renders home
+  response.render('login');
+});
+
+// app.get('/:id/:itemid', (request,response) => {
+
+//   pool.connect(( err, client, done) =>{
+//       let sql = "SELECT * FROM users INNER JOIN items ON users.id = items.ownerid WHERE users.id = '"+ request.params.id + "' AND items.itemid = '" + request.params.itemid + "'";
+//       client.query(sql, (err,res) => {
+//             console.log(res.rows[0]);
+//             response.render('item', {user : res.rows[0] });
+//       });
+//     });
+
+// });
+
 app.get('/', (request, response) => {        // Renders home
   
   if (request.cookies.loggedin == "true"){   //START OF USER BLOCK
 
     pool.connect(( err, client, done) =>{
-      let sql = "SELECT * FROM users WHERE email = '" + request.cookies.email + "'";
-      client.query(sql, (err,res)=>{
-            console.log(res.rows[0]);
-            response.render('dashboard', {user : res.rows[0] });
+      let sql = "SELECT * FROM users INNER JOIN items ON users.id = items.ownerid WHERE users.id = '"+ request.cookies.userid + "'";
+      client.query(sql, (err,res) => {
+            //console.log(res.rows);
+            response.render('dashboard', {user : res.rows });
       });
     });
 
-
   } else {  // END OF USER BLOCK
-
     response.render('home');
   }
 });
 
-app.get('/login', (request, response) => {         // Renders home
-  response.render('login');
+app.get('/give', (request, response) => {
+  if (request.cookies.loggedin == "true"){
+  response.render('postitem');
+  } else {
+    response.redirect('/login');
+  }
 });
 
+app.post('/give', (request, response) => {
+  pool.connect( (error, client, done) => {
+    
+    let sql = "INSERT INTO items (itemname, description, imglink, ownerid, shipping) VALUES ($1, $2, $3, $4, $5)";
+    let params = [request.body.itemname, request.body.description, request.body.imglink, request.cookies.userid, request.body.shipping]
+    
+    client.query(sql, params,(err,res)=> {
+      response.redirect('/');
+    });
+  });
+});
+
+app.get('/inbox', (request,response)=>{
+  pool.connect((error, client, done) => {
+    let sql = "SELECT * FROM users U INNER JOIN messages M ON U.id = M.destid INNER JOIN users UN ON M.originid = UN.id WHERE U.id = '"+ request.cookies.userid + "'";
+    client.query(sql, (err,res) => {
+
+      let data = { 
+        inbox : res.rows.length,
+        firstname : request.cookies.firstname,
+        row : res.rows
+      };
+      response.render('inbox', data);
+    });
+  });
+});
+
+app.get('/karma', (request, response) => {
+  pool.connect( (error, client, done) => {
+    let sql = "SELECT * FROM users WHERE email = '" + request.cookies.email + "'";
+    client.query(sql, (err,res) => {
+      console.log(res.row);
+      let karma = res.rows[0].karma + 1;
+      console.log('new karma', karma)
+      let cmd = "UPDATE users SET karma = '" + karma + "' WHERE email = '" + request.cookies.email + "' RETURNING *";
+      client.query(cmd, (err, result) => {
+        if (err) console.log(err);
+        response.send(result);
+        done();
+      });
+    });
+  });
+});   //PROBLEM WITH MULTI-SQL queries
+
+
 app.get('/logout', (request, response) => {         // Renders home
-  request.cookies.loggedin = "false"
-  request.cookies.email = undefined;
+  response.clearCookie('email');
+  response.clearCookie('loggedin'); 
   response.render('home');
 });
 
@@ -73,7 +142,7 @@ app.post('/login', (request, response) => { // Registration Route
 
   pool.connect( ( error , client , done )  => {     // Stores into DB users
 
-    let sql = "SELECT * from users WHERE email = '" + request.body.email + "'";
+    let sql = "SELECT * FROM users WHERE email = '" + request.body.email + "'";
 
       client.query(sql, (err, res) => {
         if (err) console.log(err);
@@ -85,6 +154,8 @@ app.post('/login', (request, response) => { // Registration Route
             if (result === true) {
               response.cookie('loggedin', true);
               response.cookie('email', request.body.email);
+              response.cookie('userid', res.rows[0].id);
+              response.cookie('firstname', res.rows[0].firstname);
               response.redirect('/'); //Pass - Cookie! - Redirect.
           } else {
             response.send("Log in fail, <a href='/login'>Try again</a>");
@@ -95,7 +166,6 @@ app.post('/login', (request, response) => { // Registration Route
       });
   });
 });
-
 
 app.get('/register', (request, response) => { // Routes to register page
   response.render('register');
@@ -109,7 +179,7 @@ app.post('/register', (request, response) => { // Registration Route
 
     pool.connect( ( error , client , done )  => {     // Stores into DB users
 
-      let sql = "INSERT INTO users (firstName, lastName, pwdhash, email) VALUES ($1, $2, $3, $4)"
+      let sql = "INSERT INTO users (firstname, lastname, pwdhash, email) VALUES ($1, $2, $3, $4)"
 
       client.query(sql, params, (err, res) => {
         if (err) console.log(err);
--- usertable.sql
+++ usertable.sql
@@ -1,7 +1,7 @@
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
-    firstName TEXT NOT NULL,
-    lastName TEXT NOT NULL,
+    firstname TEXT NOT NULL,
+    lastname TEXT NOT NULL,
     pwdhash TEXT NOT NULL,
     email TEXT NOT NULL,
     karma INT,
@@ -12,5 +12,34 @@ CREATE TABLE messages (
     id SERIAL PRIMARY KEY,
     originId INT NOT NULL,
     destId INT NOT NULL,
-    message TEXT NOT NULL
+    message TEXT NOT NULL,
+    read BOOLEAN NOT NULL
 );
+
+CREATE TABLE test (
+    id SERIAL PRIMARY KEY,
+    hits INT NOT NULL,
+);
+
+CREATE TABLE items (
+    id SERIAL PRIMARY KEY,
+    itemname VARCHAR(255) NOT NULL,
+    description TEXT,
+    imglink VARCHAR(255),
+    ownerid INT,
+    shipping TEXT
+);
+
+SELECT * FROM users U INNER JOIN messages M ON U.id = M.destid;
+
+SELECT * FROM users INNER JOIN messages ON users.id = messages.destid;
+
+SELECT
+    U.*,
+    M.originid,
+    UN.firstname,
+    M.message
+FROM users U
+INNER JOIN messages M ON U.id = M.destid
+INNER JOIN users UN ON M.originid = UN.id
+WHERE U.id = 1;
