e7dcccdfce41a01931d1d69c70562448ec0c8618
scott88lee <scott88lee@gmail.com>
2018-04-06 17:37:04 +0800

    made changes 4 heroku

--- index.js
+++ index.js
@@ -14,13 +14,49 @@ const handlebarsConfig = {
 };
 //const methodOverride = require('method-override');
 
+/////////////////
+// HEROKU CODE //
+// inside of db.js
+
+//require the url library
+//this comes with node, so no need to yarn add
+const url = require('url');
+
+//check to see if we have this heroku environment variable
+if( process.env.DATABASE_URL ){
+
+  //we need to take apart the url so we can set the appropriate configs
+
+  const params = url.parse(process.env.DATABASE_URL);
+  const auth = params.auth.split(':');
+
+  //make the configs object
+  var configs = {
+    user: auth[0],
+    password: auth[1],
+    host: params.hostname,
+    port: params.port,
+    database: params.pathname.split('/')[1],
+    ssl: true
+  };
+
+}else{
+
+  //otherwise we are on the local network
+  var configs = {
+      user: 'scottlee',
+      host: '127.0.0.1',
+      database: 'freebay',
+      port: 5432
+  };
+}
+
+//this is the same
+const pool = new pg.Pool(configs);
+// HEROKU CODE //
+/////////////////
+
 // Initialise postgres client
-const pool = new Pool({
-user: 'scottlee',
-host: '127.0.0.1',
-database: 'freebay',
-port: 5432,
-});
 
 pool.on('error', (err, client) => {
   console.error('Unexpected error on idle client', err)
@@ -244,4 +280,7 @@ app.get('/:id/:itemid', (request,response) => {
 /** ================================
  * Listen to requests on port 3000
  * =================================*/
-app.listen(3000, () => console.log('Listen Port: 3000'));
+//app.listen(3000, () => console.log('Listen Port: 3000'));
+const PORT = process.env.PORT || 3000;
+
+const server = app.listen(PORT, () => console.log('Listen port: '+PORT+''));
